f21556b54762da25c9554f7dc6a1940a51b3a5c3
Sarah Wimark <sarahwimark@gmail.com>
2018-12-29 18:49:18 +0100

    Adding tooltip functionality

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,43 @@
 class Tooltip extends HTMLElement {
     constructor() {
-       console.log('It is working')
+        super()
+        this._tooltipContainer
+        this._toolTipText = 'Default tooltip text.'
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.innerHTML = /*html*/`
+        <style>
+            div {
+                background-color: black;
+                color: white;
+                position: absolute;
+                z-index: 10;
+            }
+        </style>
+        <slot>Default slot text</slot>
+        <span> (?)</span>
+        `
+    }
+
+    connectedCallback() {
+        if(this.hasAttribute('text')) {
+            this._toolTipText = this.getAttribute('text')
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span')
+        tooltipIcon.textContent = ' (?)'
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+        this.shadowRoot.appendChild(tooltipIcon)
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div')
+        this._tooltipContainer.textContent = this._toolTipText
+        this.shadowRoot.appendChild(this._tooltipContainer)
+    }
+
+    _hideTooltip() {
+        this.shadowRoot.removeChild(this._tooltipContainer)
     }
 }
 
-customElements.define('my-tooltip', Tooltip)
+customElements.define('sw-tooltip', Tooltip)
